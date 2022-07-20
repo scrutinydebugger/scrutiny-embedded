cf9f4da088fc46d4d8c3f56235c68623bcb0a4e1
Pier-Yves Lessard <py.lessard@gmail.com>
2022-07-20 19:21:12 -0400

    Fixed memdump script

--- projects/testapp/scripts/make_testapp_memdump.sh
+++ projects/testapp/scripts/make_testapp_memdump.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-MEMRANGE="$(python3 $SCRIPT_DIR/get_elf_memranges.py ./build/example/testapp)"
-./build/testapp/testapp $MEMRANGE > testapp_memdump
+MEMRANGE="$(python3 $SCRIPT_DIR/get_elf_memranges.py ./build/projects/testapp)"
+./build/projects/testapp $MEMRANGE > testapp_memdump
