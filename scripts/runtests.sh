94c05b7d8fe069cd3de261875684a0ea52e6c312
Pier-Yves Lessard <py.lessard@gmail.com>
2022-07-30 22:51:17 -0400

    test CI

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 APP_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
-
-BUILD_DIR="$APP_ROOT/build"
+BUILD_CONTEXT="${BUILD_CONTEXT:-dev}"
+BUILD_DIR="$APP_ROOT/build-${BUILD_CONTEXT}"
 
 set -x
 
