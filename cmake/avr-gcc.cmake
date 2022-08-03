d62bcfc08150e088da9c93e004f3d070401a5b48
Pier-Yves Lessard <py.lessard@gmail.com>
2022-08-02 23:21:31 -0400

    no need for quote

--- cmake/avr-gcc.cmake
+++ cmake/avr-gcc.cmake
@@ -1,7 +1,7 @@
 find_program(GCC avr-gcc REQUIRED)
 find_program(GXX avr-g++ REQUIRED)
 
-set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(CMAKE_SYSTEM_NAME Generic)  # Needs to be set explicitely for CMAKE_CROSS_COMPILING to be True
 set(CMAKE_SYSTEM_PROCESSOR avr) 
 
