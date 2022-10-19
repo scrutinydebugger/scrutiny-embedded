3d11fcb4a418f9ca506367de4260a0e53ce95575
Pier-Yves Lessard <py.lessard@gmail.com>
2022-10-18 22:28:35 -0400

    update sfd script

--- projects/testapp/scripts/make-sfd.sh
+++ projects/testapp/scripts/make-sfd.sh
@@ -6,19 +6,17 @@ set -u
 
 WORKDIR=$(mktemp -d)
 BINFILE=$1
+TAGGED_FILE="$(dirname ${BINFILE})/$(basename ${BINFILE})_tagged"
 SFDOUTPUT=$2
 LOGLEVEL=debug
 
-scrutiny get-firmware-id "${BINFILE}" --output "${WORKDIR}" --apply  --loglevel ${LOGLEVEL}
+scrutiny get-firmware-id "${BINFILE}" --output "${WORKDIR}"  --loglevel ${LOGLEVEL}
 scrutiny elf2varmap "${BINFILE}" --output "${WORKDIR}" --loglevel ${LOGLEVEL}
 scrutiny make-metadata  --output "${WORKDIR}" --project-name "Scrutiny TestAPP" --author $(whoami) --version "1.0.0" --loglevel ${LOGLEVEL}
+scrutiny add-alias "${WORKDIR}" --file "${SCRIPT_DIR}/../assets/aliases.json" --loglevel ${LOGLEVEL}
 
-scrutiny add-alias "${WORKDIR}" --target "/rpv/x5000" --fullpath "/alias/rpv5000(enable)" --loglevel ${LOGLEVEL}
-scrutiny add-alias "${WORKDIR}" --target "/rpv/x5001" --fullpath "/alias/rpv5001(counter)" --loglevel ${LOGLEVEL}
-
-scrutiny add-alias "${WORKDIR}" --target "/static/main.cpp/process_interactive_data()/enable" --fullpath "/alias/enable_var" --loglevel ${LOGLEVEL}
-scrutiny add-alias "${WORKDIR}" --target "/static/main.cpp/process_interactive_data()/counter" --fullpath "/alias/counter_var" --loglevel ${LOGLEVEL}
-scrutiny add-alias "${WORKDIR}" --target "/static/main.cpp/process_interactive_data()/counter" --fullpath "/alias/counter_var_x2+1000" --gain 2 --offset 1000 --loglevel ${LOGLEVEL}
+scrutiny tag-firmware-id "${BINFILE}" "${TAGGED_FILE}"  --loglevel ${LOGLEVEL}
+echo "Tagged file written to ${TAGGED_FILE}"
 
 scrutiny make-sfd "${WORKDIR}" "${SFDOUTPUT}" --loglevel ${LOGLEVEL}
 scrutiny install-sfd "${SFDOUTPUT}" --loglevel ${LOGLEVEL}
