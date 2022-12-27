a4cdde256073d72f006aa6dbfcda3b00c2aa9e7b
Pier-Yves Lessard <py.lessard@gmail.com>
2022-12-27 16:33:38 -0500

    Small change to runtest.sh for cygwin

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -2,6 +2,11 @@
 set -euo pipefail
 
 APP_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. >/dev/null 2>&1 && pwd )"
+
+if [[ $(uname -s) == CYGWIN* ]];then
+APP_ROOT=$(cygpath -w "$APP_ROOT")
+fi
+
 BUILD_CONTEXT="${BUILD_CONTEXT:-dev}"
 BUILD_DIR="$APP_ROOT/build-${BUILD_CONTEXT}"
 
