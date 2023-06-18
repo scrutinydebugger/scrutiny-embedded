578d12291b06a58e14457c54273599e2e97eb3c6
Pier-Yves Lessard <py.lessard@gmail.com>
2023-06-18 14:58:15 -0400

    Added working external project example

---
+++ projects/examples/cmake_external_project/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdint>
+#include "scrutiny.hpp"
+
+uint8_t rx_buffer[128];
+uint8_t tx_buffer[128];
+
+int main(int argc, char *argv[])
+{
+    scrutiny::Config config;
+    scrutiny::MainHandler scrutiny_handler;
+
+    config.set_buffers(rx_buffer, sizeof(rx_buffer), tx_buffer, sizeof(tx_buffer));
+    scrutiny_handler.init(&config);
+
+    std::cout << std::endl
+              << "-> You made it! Project built and linked to Scrutiny successfully." << std::endl
+              << std::endl;
+    return 0;
+}
