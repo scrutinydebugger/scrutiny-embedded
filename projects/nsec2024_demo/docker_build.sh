a5226998e5fa93aa46eb9adb9d38b8265678a516
Pier-Yves Lessard <py.lessard@gmail.com>
2024-04-05 23:04:05 -0400

    initial commit

---
+++ projects/nsec2024_demo/docker_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_DIR=$(realpath "$(dirname ${BASH_SOURCE[0]})")
+cd $SCRIPT_DIR
+WORKDIR=/tmp/nsec2024
+
+docker build . -t nsec2024
+docker run \
+    --volume $SCRIPT_DIR:$WORKDIR \
+    -e ARDUINO_PATH=/usr/share/arduino \
+    nsec2024 \
+    bash -c $WORKDIR/build.sh
