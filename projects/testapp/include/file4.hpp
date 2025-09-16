4d63c621c21eddc469d3dae967ab9d8b2c2c8afb
Pier-Yves Lessard <py.lessard@gmail.com>
2025-09-15 22:23:41 -0400

    Missing files

---
+++ projects/testapp/include/file4.hpp
@@ -0,0 +1,55 @@
+
+
+#include <stdint.h>
+
+namespace File4NamespaceA1
+{
+    enum class File4EnumA : uint16_t
+    {
+        XXX = 123,
+        YYY = 456
+    };
+
+    namespace File4NamespaceA2
+    {
+        class File4ClassA1
+        {
+          public:
+            uint32_t xxx;
+            int32_t yyy[2][3];
+        };
+
+        class File4ClassA2 : public File4ClassA1
+        {
+          public:
+            uint16_t zzz[4][5][6];
+            File4EnumA A2enum;
+        };
+
+        class File4ClassA3
+        {
+          public:
+            union
+            {
+                uint32_t u32;
+                uint8_t u8[4];
+            } the_union;
+        };
+    } // namespace File4NamespaceA2
+} // namespace File4NamespaceA1
+
+namespace File4NamespaceB
+{
+    class File4ClassB
+    {
+      public:
+        bool some_bool;
+        File4NamespaceA1::File4NamespaceA2::File4ClassA2 array_of_A2[3][2];
+    };
+} // namespace File4NamespaceB
+
+extern File4NamespaceB::File4ClassB file4classB;
+extern File4NamespaceB::File4ClassB file4classB_array[3];
+extern File4NamespaceA1::File4NamespaceA2::File4ClassA3 file4classA3_array[2][2];
+
+void file4SetValues();
---
+++ projects/testapp/src/file4.cpp
@@ -0,0 +1,53 @@
+#include "file4.hpp"
+
+File4NamespaceB::File4ClassB file4classB;
+File4NamespaceB::File4ClassB file4classB_array[3];
+File4NamespaceA1::File4NamespaceA2::File4ClassA3 file4classA3_array[2][2];
+
+void file4SetValues()
+{
+    file4classB.some_bool = true;
+    file4classB.array_of_A2[2][0].xxx = 0xdeadbeef;
+    file4classB.array_of_A2[2][0].yyy[0][0] = 0x10001111;
+    file4classB.array_of_A2[2][0].yyy[0][1] = 0x20002222;
+    file4classB.array_of_A2[2][0].yyy[0][2] = 0x30003333;
+    file4classB.array_of_A2[2][0].yyy[1][2] = 0x40004444;
+    file4classB.array_of_A2[2][0].zzz[0][0][0] = 0x5566;
+    file4classB.array_of_A2[2][0].zzz[1][2][3] = 0x6789;
+    file4classB.array_of_A2[2][0].A2enum = File4NamespaceA1::File4EnumA::YYY;
+
+    file4classB_array[0].some_bool = true;
+    file4classB_array[0].array_of_A2[2][0].xxx = 0xdeadbeef + 1;
+    file4classB_array[0].array_of_A2[2][0].yyy[0][0] = 0x10001111 + 1;
+    file4classB_array[0].array_of_A2[2][0].yyy[0][1] = 0x20002222 + 1;
+    file4classB_array[0].array_of_A2[2][0].yyy[0][2] = 0x30003333 + 1;
+    file4classB_array[0].array_of_A2[2][0].yyy[1][2] = 0x40004444 + 1;
+    file4classB_array[0].array_of_A2[2][0].zzz[0][0][0] = 0x5566 + 1;
+    file4classB_array[0].array_of_A2[2][0].zzz[1][2][3] = 0x6789 + 1;
+    file4classB_array[0].array_of_A2[2][0].A2enum = File4NamespaceA1::File4EnumA::XXX;
+
+    file4classB_array[1].some_bool = true;
+    file4classB_array[1].array_of_A2[2][0].xxx = 0xdeadbeef + 2;
+    file4classB_array[1].array_of_A2[2][0].yyy[0][0] = 0x10001111 + 2;
+    file4classB_array[1].array_of_A2[2][0].yyy[0][1] = 0x20002222 + 2;
+    file4classB_array[1].array_of_A2[2][0].yyy[0][2] = 0x30003333 + 2;
+    file4classB_array[1].array_of_A2[2][0].yyy[1][2] = 0x40004444 + 2;
+    file4classB_array[1].array_of_A2[2][0].zzz[0][0][0] = 0x5566 + 2;
+    file4classB_array[1].array_of_A2[2][0].zzz[1][2][3] = 0x6789 + 2;
+    file4classB_array[1].array_of_A2[2][0].A2enum = File4NamespaceA1::File4EnumA::XXX;
+
+    file4classB_array[1].some_bool = false;
+    file4classB_array[1].array_of_A2[2][0].xxx = 0xdeadbeef + 3;
+    file4classB_array[1].array_of_A2[2][0].yyy[0][0] = 0x10001111 + 3;
+    file4classB_array[1].array_of_A2[2][0].yyy[0][1] = 0x20002222 + 3;
+    file4classB_array[1].array_of_A2[2][0].yyy[0][2] = 0x30003333 + 3;
+    file4classB_array[1].array_of_A2[2][0].yyy[1][2] = 0x40004444 + 3;
+    file4classB_array[1].array_of_A2[2][0].zzz[0][0][0] = 0x5566 + 3;
+    file4classB_array[1].array_of_A2[2][0].zzz[1][2][3] = 0x6789 + 3;
+    file4classB_array[1].array_of_A2[2][0].A2enum = File4NamespaceA1::File4EnumA::YYY;
+
+    file4classA3_array[0][0].the_union.u32 = 0x12345678;
+    file4classA3_array[0][1].the_union.u32 = 0xAABBCCDD;
+    file4classA3_array[1][0].the_union.u32 = 0x11223344;
+    file4classA3_array[1][1].the_union.u32 = 0x55667788;
+}
