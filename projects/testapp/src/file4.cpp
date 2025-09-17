61efb523da4beea54ef7b650c956b02fbc3198d7
Pier-Yves Lessard <py.lessard@gmail.com>
2025-09-16 21:07:18 -0400

    fix file4.cpp

--- projects/testapp/src/file4.cpp
+++ projects/testapp/src/file4.cpp
@@ -36,15 +36,15 @@ void file4SetValues()
     file4classB_array[1].array_of_A2[2][0].zzz[1][2][3] = 0x6789 + 2;
     file4classB_array[1].array_of_A2[2][0].A2enum = File4NamespaceA1::File4EnumA::XXX;
 
-    file4classB_array[1].some_bool = false;
-    file4classB_array[1].array_of_A2[2][0].xxx = 0xdeadbeef + 3;
-    file4classB_array[1].array_of_A2[2][0].yyy[0][0] = 0x10001111 + 3;
-    file4classB_array[1].array_of_A2[2][0].yyy[0][1] = 0x20002222 + 3;
-    file4classB_array[1].array_of_A2[2][0].yyy[0][2] = 0x30003333 + 3;
-    file4classB_array[1].array_of_A2[2][0].yyy[1][2] = 0x40004444 + 3;
-    file4classB_array[1].array_of_A2[2][0].zzz[0][0][0] = 0x5566 + 3;
-    file4classB_array[1].array_of_A2[2][0].zzz[1][2][3] = 0x6789 + 3;
-    file4classB_array[1].array_of_A2[2][0].A2enum = File4NamespaceA1::File4EnumA::YYY;
+    file4classB_array[2].some_bool = false;
+    file4classB_array[2].array_of_A2[2][0].xxx = 0xdeadbeef + 3;
+    file4classB_array[2].array_of_A2[2][0].yyy[0][0] = 0x10001111 + 3;
+    file4classB_array[2].array_of_A2[2][0].yyy[0][1] = 0x20002222 + 3;
+    file4classB_array[2].array_of_A2[2][0].yyy[0][2] = 0x30003333 + 3;
+    file4classB_array[2].array_of_A2[2][0].yyy[1][2] = 0x40004444 + 3;
+    file4classB_array[2].array_of_A2[2][0].zzz[0][0][0] = 0x5566 + 3;
+    file4classB_array[2].array_of_A2[2][0].zzz[1][2][3] = 0x6789 + 3;
+    file4classB_array[2].array_of_A2[2][0].A2enum = File4NamespaceA1::File4EnumA::YYY;
 
     file4classA3_array[0][0].the_union.u32 = 0x12345678;
     file4classA3_array[0][1].the_union.u32 = 0xAABBCCDD;
