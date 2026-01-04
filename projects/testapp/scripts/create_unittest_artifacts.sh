a0c3063250ff94d65c87151b4cb8a11409a1140c
Pier-Yves Lessard <py.lessard@gmail.com>
2026-01-03 21:34:59 -0500

    Add pointer info to testapp (#90)
    - Added pointer data to testapp (file5.cpp)
    - Added a script to generate testapp artifacts more easily

---
+++ projects/testapp/scripts/create_unittest_artifacts.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_ROOT="${SCRIPT_DIR}/../../.."
+
+cd ${PROJECT_ROOT}
+
+OUTPUT_FOLDER=unittest_outputs
+rm -rf "$OUTPUT_FOLDER"
+mkdir "$OUTPUT_FOLDER"
+export BUILD_CONTEXT=dev
+COMPILED_FILE_PATH="build-${BUILD_CONTEXT}/projects/testapp/testapp"
+
+for DWARF_VERSION in 2 3 4; do
+    rm -rf build-${BUILD_CONTEXT}
+    SCRUTINY_BUILD_TESTAPP=1 SCRUTINY_TESTAPP_DWARF_VERSION=${DWARF_VERSION} ./scripts/build.sh -DCMAKE_CXX_COMPILER=g++ -DCMAKE_C_COMPILER=gcc
+    filename=testapp-dwarf-gcc-dwarf${DWARF_VERSION}
+    filepath="$OUTPUT_FOLDER/$filename"
+    cp "$COMPILED_FILE_PATH" "$filepath"
+    ${SCRIPT_DIR}/make_testapp_memdump.sh "$filepath"
+
+    rm -rf build-${BUILD_CONTEXT}
+    SCRUTINY_BUILD_TESTAPP=1 SCRUTINY_TESTAPP_DWARF_VERSION=${DWARF_VERSION} ./scripts/build.sh -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang
+    filename=testapp-dwarf-clang-dwarf${DWARF_VERSION}
+    filepath="$OUTPUT_FOLDER/$filename"
+    cp "$COMPILED_FILE_PATH" "$filepath"
+    ${SCRIPT_DIR}/make_testapp_memdump.sh "$filepath"
+done
---
+++ projects/testapp/src/file5.cpp
@@ -0,0 +1,98 @@
+#include "file5.hpp"
+#include <stdint.h>
+
+namespace File5NamespaceA
+{
+#pragma pack(push, 1)
+    struct File5StructA
+    {
+        int32_t i32;
+        int32_t *i32_ptr;
+        uint16_t u16_array[5];
+    };
+
+    struct File5StructB
+    {
+        uint32_t u32;
+        File5StructA *structA_ptr;
+    };
+#pragma pack(pop)
+
+    int64_t file5i64;
+    int32_t file5i32;
+    int16_t file5i16;
+    int8_t file5i8;
+
+    uint64_t file5u64;
+    uint32_t file5u32;
+    uint16_t file5u16;
+    uint8_t file5u8;
+
+    File5StructA file5_structA;
+    File5StructB file5_structB;
+
+    File5StructB file5_structb_array[2];
+
+} // namespace File5NamespaceA
+
+namespace File5NamespaceB
+{
+
+    int64_t *file5i64_ptr{ nullptr };
+    int32_t *file5i32_ptr{ nullptr };
+    int16_t *file5i16_ptr{ nullptr };
+    int8_t *file5i8_ptr{ nullptr };
+
+    uint64_t *file5u64_ptr{ nullptr };
+    uint32_t *file5u32_ptr{ nullptr };
+    uint16_t *file5u16_ptr{ nullptr };
+    uint8_t *file5u8_ptr{ nullptr };
+
+    File5NamespaceA::File5StructA *file5_structA_ptr{ nullptr };
+    File5NamespaceA::File5StructB *file5_structB_ptr{ nullptr };
+    File5NamespaceA::File5StructB *file5_structB_array_ptr{ nullptr };
+
+} // namespace File5NamespaceB
+
+void file5SetValues()
+{
+    File5NamespaceA::file5i64 = -0x123456789abcdef;
+    File5NamespaceA::file5i32 = -0x77553311;
+    File5NamespaceA::file5i16 = -0x2A3C;
+    File5NamespaceA::file5i8 = -0x35;
+    File5NamespaceA::file5u64 = 0x98765432123456;
+    File5NamespaceA::file5u32 = 0xaabbccdd;
+    File5NamespaceA::file5u16 = 0xFDCE;
+    File5NamespaceA::file5u8 = 0xBD;
+
+    File5NamespaceB::file5i64_ptr = &File5NamespaceA::file5i64;
+    File5NamespaceB::file5i32_ptr = &File5NamespaceA::file5i32;
+    File5NamespaceB::file5i16_ptr = &File5NamespaceA::file5i16;
+    File5NamespaceB::file5i8_ptr = &File5NamespaceA::file5i8;
+
+    File5NamespaceB::file5u64_ptr = &File5NamespaceA::file5u64;
+    File5NamespaceB::file5u32_ptr = &File5NamespaceA::file5u32;
+    File5NamespaceB::file5u16_ptr = &File5NamespaceA::file5u16;
+    File5NamespaceB::file5u8_ptr = &File5NamespaceA::file5u8;
+
+    File5NamespaceB::file5_structA_ptr = &File5NamespaceA::file5_structA;
+    File5NamespaceB::file5_structB_ptr = &File5NamespaceA::file5_structB;
+    File5NamespaceB::file5_structB_array_ptr = File5NamespaceA::file5_structb_array;
+
+    File5NamespaceA::file5_structA.i32 = 31415926;
+    File5NamespaceA::file5_structA.i32_ptr = &File5NamespaceA::file5i32;
+    File5NamespaceA::file5_structA.u16_array[0] = 0x121;
+    File5NamespaceA::file5_structA.u16_array[1] = 0x122;
+    File5NamespaceA::file5_structA.u16_array[2] = 0x123;
+    File5NamespaceA::file5_structA.u16_array[3] = 0x124;
+    File5NamespaceA::file5_structA.u16_array[4] = 0x125;
+
+    File5NamespaceA::file5_structB.u32 = 0x97461346;
+    File5NamespaceA::file5_structB.structA_ptr = &File5NamespaceA::file5_structA;
+
+    File5NamespaceA::file5_structb_array[0].u32 = 0x1195735;
+    File5NamespaceA::file5_structb_array[0].structA_ptr = &File5NamespaceA::file5_structA;
+
+    File5NamespaceA::file5_structb_array[1].u32 = 0x1284629;
+    File5NamespaceA::file5_structb_array[1].structA_ptr = &File5NamespaceA::file5_structA;
+}
