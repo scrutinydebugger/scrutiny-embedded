e46fe1ec7e046ce9b6ea358d2d1d37ef56f036c9
Pier-Yves Lessard <py.lessard@gmail.com>
2026-06-10 20:20:48 -0400

    Code Banner update (#102)

--- projects/testapp/src/file5.cpp
+++ projects/testapp/src/file5.cpp
@@ -1,3 +1,11 @@
+//    file5.cpp
+//        Placeholder file for variable extraction test
+//
+//   - License : MIT - See LICENSE file
+//   - Project : Scrutiny Debugger (github.com/scrutinydebugger/scrutiny-embedded)
+//
+//    Copyright (c) 2021 Scrutiny Debugger
+
 #include "file5.hpp"
 #include <stdint.h>
 
