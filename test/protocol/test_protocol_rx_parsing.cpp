scrutinydebugger/scrutiny-embedded

commit d1c13de4946d126d0e147530ea69756c3f9946b1

Spread the CRC32 computation load in time (#117)
Remove unused func

lib/src/protocol/scrutiny_comm_handler.cpp
@@ -39,6 +39,7 @@ namespace scrutiny
             m_active_response.data = m_tx_buffer; // Half duplex comm. Share buffer
             m_active_response.data_max_length = m_tx_buffer_size;
             m_enabled = true;
+            m_crc = 0;
 
             if (m_rx_buffer_size < MINIMUM_RX_BUFFER_SIZE || m_rx_buffer_size > MAXIMUM_RX_BUFFER_SIZE)
             {
@@ -92,6 +93,7 @@ namespace scrutiny
                 {
                 case RxFSMState::WaitForCommand:
                 {
+                    m_crc = 0;
                     if ((data[i] & 0x80) != 0) // Invalid command
                     {
                         m_rx_error = RxError::InvalidCommand;
@@ -100,6 +102,7 @@ namespace scrutiny
                     else
                     {
                         m_active_request.command_id = data[i];
+                        m_crc = tools::crc32(&data[i], 1, m_crc);
                         m_rx_state = RxFSMState::WaitForSubfunction;
                         i += 1;
                     }
@@ -109,6 +112,7 @@ namespace scrutiny
                 case RxFSMState::WaitForSubfunction:
                 {
                     m_active_request.subfunction_id = data[i];
+                    m_crc = tools::crc32(&data[i], 1, m_crc);
                     m_rx_state = RxFSMState::WaitForLength;
                     i += 1;
                     m_per_state_data.length_bytes_received = 0;
@@ -144,6 +148,7 @@ namespace scrutiny
 
                     if (next_state)
                     {
+                        m_crc = tools::crc32(&data[i - 2], 2, m_crc);
                         if (m_active_request.data_length == 0)
                         {
                             m_per_state_data.crc_bytes_received = 0;
@@ -172,6 +177,7 @@ namespace scrutiny
                     uint16_t const data_bytes_to_read = (available_bytes >= missing_bytes) ? missing_bytes : available_bytes;
 
                     memcpy(&m_rx_buffer[m_per_state_data.data_bytes_received], &data[i], data_bytes_to_read);
+                    m_crc = tools::crc32(&data[i], data_bytes_to_read, m_crc);
                     m_per_state_data.data_bytes_received += data_bytes_to_read;
                     i += data_bytes_to_read;
 
@@ -203,7 +209,7 @@ namespace scrutiny
                         m_active_request.crc |= static_cast<uint32_t>(data[i]) << 0;
                         m_state = State::Idle;
 
-                        if (check_crc(&m_active_request))
+                        if (m_crc == m_active_request.crc)
                         {
                             process_active_request();
                         }
@@ -471,19 +477,6 @@ namespace scrutiny
             return m_nbytes_to_send - m_nbytes_sent;
         }
 
-        bool CommHandler::check_crc(Request const *const req)
-        {
-            uint32_t crc = 0;
-            unsigned char header_data[4];
-            header_data[0] = req->command_id;
-            header_data[1] = req->subfunction_id;
-            header_data[2] = (req->data_length >> 8) & 0xFF;
-            header_data[3] = req->data_length & 0xFF;
-            crc = tools::crc32(header_data, sizeof(header_data));
-            crc = tools::crc32(req->data, req->data_length, crc);
-            return (crc == req->crc);
-        }
-
         void CommHandler::add_crc(Response *const response) const
         {
             if (response->data_length > m_tx_buffer_size)
@@ -523,6 +516,7 @@ namespace scrutiny
             m_request_received = false;
             m_rx_error = RxError::None;
             m_last_rx_timestamp = m_timebase->get_timestamp();
+            m_crc = 0;
 
             if (m_state == State::Receiving)
             {

lib/inc/protocol/scrutiny_comm_handler.hpp
@@ -65,11 +65,6 @@ namespace scrutiny
             /// @brief Returns the number of bytes pending to be sent.
             uint16_t data_to_send(void) const;
 
-            /// @brief Tells if the Request object has a CRC that matches its payload content.
-            /// @param req The request to check
-            /// @return True if the CRC is valid. False otherwise
-            static bool check_crc(Request const *const req);
-
             // Writes the CRC property of the response based on the payload content.
             void add_crc(Response *const response) const;
 
@@ -203,6 +198,7 @@ namespace scrutiny
             uint16_t m_nbytes_to_send;    // Number of bytes to send in this response
             uint16_t m_nbytes_sent;       // Number of bytes sent up to now. Includes headers and CRC
             TxError::eTxError m_tx_error; // Last Transmission error code
+            uint32_t m_crc;               // CRC of the incoming data computed has bytes come in
 
           private:
             static uint32_t s_session_counter; // A counter to generate session ID

lib/src/scrutiny_tools.cpp
@@ -153,5 +153,6 @@ namespace scrutiny
             }
             return ~crc;
         }
+
     } // namespace tools
 } // namespace scrutiny

commit 979a4b40c9fa844594568804c49bd553e486d285

Fix CRC computation broken in #117  (#123)

lib/src/protocol/scrutiny_comm_handler.cpp
@@ -127,6 +127,7 @@ namespace scrutiny
                         if ((len - i) >= 2)
                         {
                             m_active_request.data_length = (static_cast<uint16_t>(data[i]) << 8u) | (static_cast<uint16_t>(data[i + 1]));
+                            m_crc = tools::crc32(&data[i], 2, m_crc);
                             m_per_state_data.length_bytes_received = 2;
                             i += 2;
                             next_state = true;
@@ -134,6 +135,7 @@ namespace scrutiny
                         else
                         {
                             m_active_request.data_length = static_cast<uint16_t>(data[i]) << 8u;
+                            m_crc = tools::crc32(&data[i], 1, m_crc);
                             m_per_state_data.length_bytes_received = 1;
                             i += 1;
                         }
@@ -141,6 +143,7 @@ namespace scrutiny
                     else
                     {
                         m_active_request.data_length |= static_cast<uint16_t>(data[i]);
+                        m_crc = tools::crc32(&data[i], 1, m_crc);
                         m_per_state_data.length_bytes_received = 2;
                         i += 1;
                         next_state = true;
@@ -148,7 +151,6 @@ namespace scrutiny
 
                     if (next_state)
                     {
-                        m_crc = tools::crc32(&data[i - 2], 2, m_crc);
                         if (m_active_request.data_length == 0)
                         {
                             m_per_state_data.crc_bytes_received = 0;

test/protocol/test_protocol_rx_parsing.cpp
@@ -52,6 +52,7 @@ TEST_F(TestRxParsing, TestRx_ZeroLen_BytePerByte)
     for (unsigned int i = 0; i < sizeof(data); i++)
     {
         comm.receive_data(&data[i], 1);
+        data[i] ^= 0xFF; // destroy previous data
     }
 
     ASSERT_TRUE(comm.request_received());
@@ -91,7 +92,30 @@ TEST_F(TestRxParsing, TestRx_NonZeroLen_BytePerByte)
     for (unsigned int i = 0; i < sizeof(data); i++)
     {
         comm.receive_data(&data[i], 1);
+        data[i] ^= 0xFF; // Destroy previous data
     }
+
+    ASSERT_TRUE(comm.request_received());
+    scrutiny::protocol::Request const *req = comm.get_request();
+    EXPECT_EQ(req->command_id, 1);
+    EXPECT_EQ(req->subfunction_id, 2);
+    EXPECT_EQ(req->data_length, 3);
+    EXPECT_EQ(req->data[0], 0x11);
+    EXPECT_EQ(req->data[1], 0x22);
+    EXPECT_EQ(req->data[2], 0x33);
+
+    EXPECT_EQ(comm.get_rx_error(), scrutiny::protocol::RxError::None);
+}
+
+//=============================================================================
+TEST_F(TestRxParsing, TestRx_NonZeroLen_SplitInLen)
+{
+    unsigned char data[11] = { 1, 2, 0, 3, 0x11, 0x22, 0x33 };
+    add_crc(data, 7);
+
+    comm.receive_data(&data[0], 3); // cmd
+    memset(data, 0xFF, 3);          // Destroy data to catch negative indexing
+    comm.receive_data(&data[3], 8); // data
 
     ASSERT_TRUE(comm.request_received());
     scrutiny::protocol::Request const *req = comm.get_request();